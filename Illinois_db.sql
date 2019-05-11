e535837bde389ccb71c742ec7c6cc8858402a2bc
Rishi Hebbar <rishi.hebbar@gmail.com>
2019-05-11 12:44:55 -0500

    push

---
+++ Illinois_db.sql
@@ -0,0 +1,15 @@
+create database illinois_db;
+
+CREATE TABLE illinois_data (
+ County TEXT (10),
+ Population INT,
+ Income INT,
+ Poverty INT,
+ Total_Employment INT,
+ Labor_Force INT,
+ Total_Unemployment INT,
+ Unemployment_Rate INT
+);
+use illionis_db;
+
+select * from illinois_data;
